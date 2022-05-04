c437ffd3b3bad735c803eef6c23a8218d13e914a
44apach <102644780+44apach@users.noreply.github.com>
2022-05-04 14:06:00 +0300

    new site

---
+++ sauna.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+  
+    
+  
+  
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    
+    slidesPerView: 3.5,
+    loop: true,
+    centeredslides: true,
+  });
